dab57f51970aa37eab40b3094024dfdebf73ea21
Josh Weidenaar <josh.weidenaar@intersectillinois.org>
2021-03-08 00:07:49 -0600

    pybank analysis is complete with all steps working properly next step is to write to csv for pybank and then develop pypoll analysis

--- python_challenge/Pybank/main.py
+++ python_challenge/Pybank/main.py
@@ -6,6 +6,8 @@ counter = 0
 tot = 0 
 prolo_prev = 867884  
 prolo_list = []
+prolo_year_list = []
+prolo_prolo_list = []
 
 # define path * lines 5 - 22 pulled from python day 2 lesson 8*
 csv_path = os.path.join('Resources', 'budget_data.csv')
@@ -15,11 +17,10 @@ with open(csv_path) as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    print(csvreader)
 
     # Reads header row
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    #print(f"CSV Header: {csv_header}")
 
     
     # Read each row of data after the header
@@ -27,6 +28,8 @@ with open(csv_path) as csvfile:
         
         date = i[0]
         prof_los = i[1]
+        prolo_year_list.append(date)
+        prolo_prolo_list.append(i[1])
         # Change for the month appending to list
         prolo_change = prolo_prev - float(prof_los)
         prolo_list.append(float(prolo_change))
@@ -37,12 +40,24 @@ with open(csv_path) as csvfile:
             counter = counter + 1
         # Sum up total profits/losses
         tot = int(prof_los) + tot
-    
+    #iterate through list made to add change together then divide by # of months - 1 for one fewer instance of change
     prolo_change = 0
+    #finding biggest change in profits/losses to then print out corresponding values in other lists (negative for increase and positive for decrease)
+    index_change_loss = prolo_list.index(max(prolo_list))
+    index_change_Gain = prolo_list.index(min(prolo_list))
+
+    #iterate through prololist to get avg change
     for i in prolo_list:
         prolo_change = prolo_change + i
-        print(prolo_change)
     av_prolo = prolo_change/(counter - 1)
+
+    #print all my found values
+    print("FINANCIAL ANALYSIS")
+    print("-------------------------------------------------------------------------------------------------")
     print(f"Months in dataset: {counter}")
     print(f"Total profit/loss: ${tot}")
-    print(f"Average profit/loss: ${av_prolo}")
+    print(f"Average monthly change in profit/loss: ${av_prolo}")
+    print(f"In {prolo_year_list[index_change_Gain]} the firm made ${abs(prolo_list[index_change_Gain])} dollars, the greatest increase in profits in this series.")
+    print(f"In {prolo_year_list[index_change_loss]} the firm lost ${prolo_list[index_change_loss]} dollars, the greatest decrease in profits in this series.")
+    
+    # next step is to write to csv for pybank and then develop pypoll analysis
